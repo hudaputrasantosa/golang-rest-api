d110227a22088dcbcddb9761db41845e9a22ed6c
hudaputrasantosa <hudaputrasantosa@gmail.com>
2024-01-27 21:32:47 +0700

    update: setup router

--- main.go
+++ main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
-	"net/http"
-
+	helmet "github.com/danielkov/gin-helmet"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
-	})
+	// r.GET("/", func(c *gin.Context) {
+	// 	c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+	// })
 
 	r.Run()
+}
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"*"},
+		AllowHeaders:  []string{"*"},
+		AllowWildcard: true,
+	}))
+
+	router.Use(helmet.Default())
+	routes.initUserRoute(router)
+	return router
 }
