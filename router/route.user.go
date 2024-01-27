b100fed32dba19b0f67b09f441b3f519ed1b42ad
hudaputrasantosa <hudaputrasantosa@gmail.com>
2024-01-27 21:32:17 +0700

    add: routing

--- router/route.user.go
+++ router/route.user.go
@@ -1,1 +1,17 @@
-package models
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initUserRoute(route *gin.Engine) {
+	groupRoute := route.Group("api/v1")
+
+	groupRoute.GET("/users", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Get User!",
+		})
+	})
+}
